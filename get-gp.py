40bd46380322c2d94e1a2fa62a891a8e0d8f66f7
nodrog1061 <59317189+nodrog1061@users.noreply.github.com>
2021-04-09 19:30:39 +0100

    Update get-gp.py
    added comments

--- get-gp.py
+++ get-gp.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 def getData():
+    #extracts the config and compoles it to non prety mode
     with open('config.json') as json_file:
         lines = json_file.readlines()
         json_file = '\t'.join([line.strip() for line in lines])
@@ -9,12 +10,15 @@ def getData():
         config = json.loads(json_file)
         #print(config["requestaddress"])
 
+    #sends the get request to the server
     data = requests.get(config["requestaddress"])
     #print(data.text)
+    #tells py its a json file
     data = json.loads(data.text)
     #print(data["time"])
     #{"voltage":19,"voltsLower":11,"current":20,"speed":12,"throttle":98,"rpm":1680,"time":"17:07:59","lat":"50.8575914","lon":"-0.7576133","temp1":"16.2","temp2":"15.9","track":"Goodwood","currLap":4.1916603248984625}
 
+    #sets each json item as a varible for later
     voltage = data["voltage"]
     voltsLower = data["voltsLower"]
     current = data["current"]
@@ -29,8 +33,10 @@ def getData():
     track = data["track"]
     currLap = data["currLap"]
     
+    #sends all the varibles back
     return voltage,voltsLower,current,speed,throttle,rpm,time,lat,lon,temp1,temp2,track,currLap
 
-voltage,voltsLower,current,speed,throttle,rpm,time,lat,lon,temp1,temp2,track,currLap = getData()
+#debug
+""" voltage,voltsLower,current,speed,throttle,rpm,time,lat,lon,temp1,temp2,track,currLap = getData()
 
-print(time)
+print(time) """
